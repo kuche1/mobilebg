46613eee9e909770399ad7c67ccd0b25abbcbc6e
kuche1 <znamsiparolata4esno@abv.bg>
2025-08-31 16:42:39 +0300

    update

--- mobilebg.py
+++ mobilebg.py
@@ -7,7 +7,8 @@ from dataclasses import dataclass
 import requests_cache
 from pathlib import Path
 
-URL = 'https://www.mobile.bg/obiavi/avtomobili-dzhipove/namira-se-v-balgariya/p-{page_num}?price={price_min}&price1={price_max}&sort=6&nup=014&pictonly=1'
+MOBILE_PREFIX = 'https://www.mobile.bg/'
+URL = MOBILE_PREFIX + 'obiavi/avtomobili-dzhipove/namira-se-v-balgariya/p-{page_num}?price={price_min}&price1={price_max}&sort=6&nup=014&pictonly=1'
 # `sort=6` - sort by newest
 # `pictonly=1` - only show if picture is available
 
@@ -18,17 +19,92 @@ BS_PARSER = 'html.parser'
 NET_CACHE_DURATION_SEC = 60 * 60 * 24
 NET_CACHE_LOC = str(Path(__file__).parent / "cache")
 
-@dataclass(kw_only=True)
+##########
+########## class
+##########
+
+@dataclass
 class Car:
-    url: str
+    link_mobile: str
+    link_autodata: str
+
     title: str
+    fuel_consumption_urban: float # in liters
+    fuel_consumption_highway: float # in liters
+
+    @classmethod
+    def new(cls, link_mobile: str, link_autodata: str, title: str) -> "Car":
+        # print()
+        # print(f'dbg: {link_mobile=}')
+        # print(f'dbg: {link_autodata=}')
+
+        autodata_html = net_req(link_autodata)
+        soup = BeautifulSoup(autodata_html, BS_PARSER)
+
+        fuel_consumption_urban = extract_fuel_consumption(soup, 'Разход на гориво - градско')
+        fuel_consumption_highway = extract_fuel_consumption(soup, 'Разход на гориво - извънградско')
+        if (fuel_consumption_urban is None) or (fuel_consumption_highway is None):
+            print(f'WARNING: could not extract fuel consumption for: {link_mobile}')
+            fuel_consumption_urban = float('inf')
+            fuel_consumption_highway = float('inf')
+
+        return cls(link_mobile, link_autodata, title, fuel_consumption_urban, fuel_consumption_highway)
+
+##########
+########## network
+##########
 
 # TODO: actually cache this (at least for a couple of hours) (maybe use MCT ? btw MCT I think needs to be reworked to exclude that hashing shit, it also needs some safety atomic operations)
 def net_req(url: str) -> str:
     response = requests.get(url)
-    response.encoding = "cp1251" # otherwise we get gibberish
+
+    if url.startswith(MOBILE_PREFIX):
+        response.encoding = "cp1251" # otherwise we get gibberish
+
     assert response.ok
     return response.text
+
+##########
+########## html extract
+##########
+
+def extract_fuel_consumption(soup: BeautifulSoup, prefix: str) -> float | None:
+    elem = soup.find('td', string=prefix)
+    if elem is None:
+        return None
+    fuel_consumption = elem.parent.text
+
+    fuel_consumption = fuel_consumption.replace('\n', ' ').replace('\t', ' ')
+    while '  ' in fuel_consumption:
+        fuel_consumption = fuel_consumption.replace('  ', ' ')
+    fuel_consumption = fuel_consumption.strip()
+
+    tmp = prefix + ' '
+    assert fuel_consumption.startswith(tmp)
+    fuel_consumption = fuel_consumption.removeprefix(tmp)
+
+    tmp = ' Литра/100 км'
+    assert fuel_consumption.endswith(tmp)
+    fuel_consumption = fuel_consumption.removesuffix(tmp)
+
+    if ' 'in fuel_consumption:
+        part1, part2 = fuel_consumption.split(' ')
+        
+        part1 = float(part1)
+
+        assert part2.startswith('(')
+        part2 = part2[1:]
+
+        assert part2.endswith(')')
+        part2 = part2[:-1]
+
+        part2 = float(part2)
+
+        fuel_consumption = max(part1, part2)
+
+    fuel_consumption = float(fuel_consumption)
+
+    return fuel_consumption
 
 def extract_car_links_from_website(*, number_of_pages_to_extract: int) -> list[str]:
     car_links = []
@@ -57,13 +133,22 @@ def extract_cars_data_from_links(links: list[str]):
         soup = BeautifulSoup(car_html, BS_PARSER)
 
         elem_title = soup.find('div', class_='obTitle')
-
         title = elem_title.text.strip()
         title = title.split(' Обява: ')[0]
 
-        cars.append(Car(url=link, title=title))
+        elem_params = soup.find('div', class_='borderBox carParams')
+        # TODO: fill whatever's important
+
+        elem_link_autodata = elem_params.find('div', class_='autodata24')
+        link_autodata = elem_link_autodata.find('a').get('href')
+
+        cars.append(Car.new(link, link_autodata, title))
 
     return cars
+
+##########
+########## main
+##########
 
 def main() -> None:
     requests_cache.install_cache(NET_CACHE_LOC, expire_after=NET_CACHE_DURATION_SEC)
